f8195b07217e5bc27059a59357e6740b613c90c3
NesterovNick <nekit.vlz@mail.ru>
2025-11-21 17:14:20 +0300

    feat: add navigation with active states and smooth scroll

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,52 @@
 // Ждем, когда вся структура DOM (наш HTML) загрузится
 document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll(".nav a");
+
+    let isManualScroll = true;
+
+    navLinks.forEach(link =>  {
+        link.addEventListener('click', function() {
+            isManualScroll = false;
+            navLinks.forEach(l => l.classList.remove('active'));
+            this.classList.add('active');
+
+            setTimeout(() => {
+                isManualScroll = true;
+            }, 1000);
+        });
+    });
+
+
+
+
+    window.addEventListener('scroll', function() {
+        if (!isManualScroll) return;
+
+    let current = '';
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.pageYOffset + (windowHeight / 2);
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        const sectionBottom = sectionTop + sectionHeight;
+        
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
+});
+
+
+
 
     // Находим ВСЕ кнопки "В корзину" на странице
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
