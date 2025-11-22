346e914a7f829a010b48a27067436565ddd1fce0
NesterovNick <nekit.vlz@mail.ru>
2025-11-22 19:07:02 +0300

    add backend and Spring boot

---
+++ backend/src/main/java/com/irichoco/cake_shop/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.irichoco.cake_shop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "String Boot работает! ^&^";
+    }
+}
